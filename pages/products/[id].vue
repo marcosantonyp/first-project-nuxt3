3554612e0ecf65bc37040ac7fd5ee47b22cb546e
Antony <marcos.antony@live.com>
2025-11-20 18:25:48 -0300

    final course

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,14 +1,26 @@
 <template>
-    <div>
-        <p>Product details for {{ id }}</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa cumque minima molestias porro quisquam nostrum ea, veritatis aspernatur magnam repudiandae ad! Minus officiis magni neque quod eos explicabo dolores repudiandae?</p>
-    </div>
+  <div>
+    <Head>
+      <Title>Nuxt Dojo | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </Head>
+    
+    <ProductDetails :product="product" />
+  </div>
 </template>
 
 <script setup>
-    const { id } = useRoute().params
+  const { id } = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
+
+  //  fetch the products
+  const { data: product } = await useFetch(uri, { key: id })
+
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found' })
+  }
+
+  definePageMeta({
+    layout: "products",
+  })
 </script>
-
-<style scoped>
-
-</style>
